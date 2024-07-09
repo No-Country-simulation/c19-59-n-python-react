3d5bc90f06dbcdeab379a8d128af82393fa25a26
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2024-07-09 19:29:06 -0300

    Creado arquitectura del proyecto

---
+++ frontend/Vet-Tech-App/src/VetTechApp.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+export const VetTechApp = () => {
+
+
+  return (
+
+    <div className="flex h-[400px] items-center justify-center">
+      <h1 className="text-emerald-400 text-[200px]">Vet-techApp</h1>
+    </div>
+  )
+}
+
+

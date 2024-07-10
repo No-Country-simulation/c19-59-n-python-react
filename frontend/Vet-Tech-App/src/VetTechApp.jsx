c1dd0a7c53966dc88b399cae1ab59b14d9299e36
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2024-07-10 20:02:37 -0300

    crear archivo store y generar el componente Provider en la parte mas alta de la app  pasandole el store.js

--- frontend/Vet-Tech-App/src/VetTechApp.jsx
+++ frontend/Vet-Tech-App/src/VetTechApp.jsx
@@ -1,3 +1,5 @@
+import { Provider } from "react-redux"
+import { store } from "./store/slices/store"
 
 
 
@@ -8,10 +10,11 @@ export const VetTechApp = () => {
 
 
   return (
-
-    <div className="flex h-[400px] items-center justify-center">
-      <h1 className="text-emerald-400 text-[200px]">Vet-techApp</h1>
-    </div>
+    <Provider store={store}>
+      <div className="flex h-[400px] items-center justify-center">
+        <h1 className="text-emerald-400 text-[200px]">Vet-techApp</h1>
+      </div>
+    </Provider>
   )
 }
 

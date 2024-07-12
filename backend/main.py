22d4a508f58a0edaa18a7c10e15d509cf101f19a
Cristian Lo Giudice <cflg.dev@gmail.com>
2024-07-11 21:35:14 -0300

    Que miras? 👀
    Cambie el retorno de la funcion root

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", tags=["Root"])
 async def root():
-    return "fas tapi"
+    return "Que miras? 👀"
 
-# Para levantar el server, recarga cada vez que se guardan cambios: uvicorn app.main:app --reload
-# Para levantar el server sin reload automatico: uvicorn app.main:app
-
-# Documentacion con swagger http://127.0.0.1:8000/docs
-# Documentacion con redoc http://127.0.0.1:8000/redoc

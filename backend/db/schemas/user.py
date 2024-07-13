32e45cbe4305b4ad3a2f585d1f23faff6da9e3d1
Cristian Lo Giudice <cflg.dev@gmail.com>
2024-07-13 20:39:06 -0300

    Correccion de user_schema para que acepte las nuevas propiedades del modelo User

--- backend/db/schemas/user.py
+++ backend/db/schemas/user.py
@@ -3,7 +3,16 @@ def user_schema(user) -> dict:
         "id": str(user["_id"]),
         "username": str(user["username"]),
         "email": str(user["email"]),
-        "active": bool(user["active"])
+        "active": bool(user["active"]),
+        "name": bool(user["name"]),
+        "last_name": bool(user["last_name"]),
+        "image": bool(user["image"]),
+        "address": bool(user["address"]),
+        "country_residence": bool(user["country_residence"]),
+        "docs": bool(user["docs"]),
+        "role": bool(user["role"]),
+        "password": bool(user["password"]),
+        "pet": bool(user["pet"])
     }
 
 def users_schema(users) -> list:

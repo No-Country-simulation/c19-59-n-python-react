87896bf75ec1dc0c40ee40b8b515a9375a08a160
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2024-07-14 16:40:07 -0300

    se agrego theme/colors en la config de tailwind

---
+++ frontend/Vet-Tech-App/src/components/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+
+
+export const PrimaryButton = (children) => {
+
+
+  return (
+    <button>{children}</button>
+  )
+}
--- frontend/Vet-Tech-App/tailwind.config.js
+++ frontend/Vet-Tech-App/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // colores de app
+        "primaryColor":"hsl(277, 62%, 51%)",
+        "secondaryColor":"hsl(299, 51%, 53%)",
+        "menuColor-1":"hsl(184, 65%, 52%)",
+        "menuColor-2":"hsl(83, 78%, 51%)",
+        "menuColor-3":"hsl(43, 92%, 57%)",
+        "menuColor-4":"hsl(353, 100%, 81%)",
+        "baseColor":"hsl(0, 0%, 90%)",
+        
+        //text
+        "whiteText":"hsl(0, 3%, 94%)",
+        "blackText":"hsl(0, 0%, 15%)",
+    },
+    },
   },
   plugins: [],
 }
